19204fac9d374f63da626594839c1ef9bcdb6239
branfts <support@june07.com>
2024-07-24 23:22:38 -0700

    Updated by A10x Service

---
+++ public/u/paula.js
@@ -0,0 +1,14 @@
+
+const links = [
+    {
+        "url": "https://www.tiktok.com/@paula.f.lorini"
+    },
+    {
+        "url": "https://www.threads.net/@paula_fernanda_lorini"
+    },
+    {
+        "url": "https://www.instagram.com/paula_fernanda_lorini/"
+    }
+];
+const rules = [];
+export { links, rules };
