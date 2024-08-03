0ccbd1907adb9f57b82e17e542a1a12644b752f3
branfts <support@june07.com>
2024-08-02 21:04:44 -0700

    Updated by A10x Service

---
+++ public/u/phidigital.js
@@ -0,0 +1,12 @@
+
+const links = [
+    { url: 'https://thedigitalhandbook.ca' },
+{ url: 'https://tiktok.com/@phi.digital' },
+{ url: 'https://www.youtube.com/@the.digital.handbook' },
+{ url: 'https://www.pinterest.ca/digitalhandbookstore/' }
+]
+const rules = [
+    
+]
+
+export { links, rules }
